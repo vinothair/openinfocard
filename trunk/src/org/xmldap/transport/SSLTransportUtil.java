a2ee0f25a99fac6606067b8c4f3c6a6e317ecea9
ian.g.brown <ian.g.brown@ae25dcb7-a91b-0410-abf2-913f48ddaf86>
2006-09-05 17:27:09 +0000

    adding a hack/util for getting server cert(s) in SSL handshake

---
+++ trunk/src/org/xmldap/transport/SSLTransportUtil.java
@@ -0,0 +1,81 @@
+package org.xmldap.transport;
+
+import com.sun.slamd.example.BlindTrustSocketFactory;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.security.cert.Certificate;
+import java.net.URL;
+import java.io.IOException;
+
+/**
+ * Blind trust SSL negotiation to retrieve (server) certs used in handshake. Uses Neil Wilson's
+ * SLAMD BlindTrustSocketFactory. There are some weird class loading issues, so be careful when
+ * deploying in the wild. In particular, class cast exceptions due to URLConnection impl for SSL
+ * connections may occur.
+ * @author igb
+ *
+ */
+public class SSLTransportUtil {
+    /**
+     *
+     * @param host (i.e. https://xmldap.org)
+     * @return server certs used in SSL handshake
+     * @deprecated (see classloader issues described above)
+     */
+    public static Certificate[] getServerCertificates(String host) throws IOException {
+         URL url  = new URL(host);
+        return getServerCertificates(url);
+
+    }
+
+    /**
+     *
+      * @param url URL of host (we presume SSL)
+     * @return server certs used in SSL handshake
+     * @throws IOException
+     * @deprecated (see classloader issues described above)
+     */
+    public static Certificate[] getServerCertificates(URL url) throws IOException {
+        HttpsURLConnection conn = (HttpsURLConnection)url.openConnection();
+
+        return getServerCertificates(conn);
+    }
+
+    /**
+     *
+     * @param conn
+     * @return server certs used in SSL handshake
+     * @throws IOException
+     */
+    public static Certificate[] getServerCertificates(HttpsURLConnection conn) throws IOException {
+        // weird classloader stuff, this needs to allways be AFTER the opening of the connection
+        //TODO: figure out wtf is going on with the classloader
+        BlindTrustSocketFactory blindTrust = null;
+        try {
+            blindTrust = new BlindTrustSocketFactory();
+        } catch (Exception e) {
+              e.printStackTrace();
+            // throws Exception? weird. but, Neil is a better prgrammer than the rest of us, so . . .
+            //TODO: dig into this later
+
+        }
+
+        conn.setSSLSocketFactory(blindTrust);
+        conn.connect();
+
+        return conn.getServerCertificates();
+    }
+
+    /**
+     * Test against http://xmldap.org
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        Certificate[] certs = SSLTransportUtil.getServerCertificates("https://xmldap.org");
+        for (int i = 0; i < certs.length; i++) {
+            Certificate cert = certs[i];
+            System.out.println(cert);
+        }
+    }
+}
