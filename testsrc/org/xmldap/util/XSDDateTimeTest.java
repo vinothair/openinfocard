d9478f1f96fcb8bd95d719efa7206c2bba436812
ignisvulpis <ignisvulpis@ae25dcb7-a91b-0410-abf2-913f48ddaf86>
2006-09-27 14:35:26 +0000

    Axel Nennker: added function to parse the datetime string. Added a test for this funktion too.

--- testsrc/org/xmldap/util/XSDDateTimeTest.java
+++ testsrc/org/xmldap/util/XSDDateTimeTest.java
@@ -1,5 +1,9 @@
 package org.xmldap.util;
 
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
 import junit.framework.TestCase;
 
 /**
@@ -16,6 +20,37 @@ public class XSDDateTimeTest extends TestCase {
     public void setUp() throws Exception {
         super.setUp();
         dateTime = new XSDDateTime();
+    }
+
+    public void testParse() throws Exception {
+    	Calendar cal = XSDDateTime.parse("2006-09-27T12:58:26Z");
+        assertEquals(2006, cal.get(Calendar.YEAR));
+        assertEquals(8, cal.get(Calendar.MONTH));
+        assertEquals(27, cal.get(Calendar.DAY_OF_MONTH));
+        assertEquals(12, cal.get(Calendar.HOUR_OF_DAY));
+        assertEquals(58, cal.get(Calendar.MINUTE));
+        assertEquals(26, cal.get(Calendar.SECOND));
+
+    	TimeZone tz = TimeZone.getTimeZone("GMT+00:00");
+        Calendar exp = Calendar.getInstance(tz, Locale.US);
+        exp.set(2006,Calendar.SEPTEMBER,27,12,58,26);
+        assertEquals(exp.get(Calendar.YEAR), cal.get(Calendar.YEAR));
+        assertEquals(exp.get(Calendar.MONTH), cal.get(Calendar.MONTH));
+        assertEquals(exp.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.DAY_OF_MONTH));
+        assertEquals(exp.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.HOUR_OF_DAY));
+        assertEquals(exp.get(Calendar.MINUTE), cal.get(Calendar.MINUTE));
+        assertEquals(exp.get(Calendar.SECOND), cal.get(Calendar.SECOND));
+    }
+
+    public void testFormat() throws Exception {
+//    	2006-09-27T12:58:26Z
+//    	2006-09-27T13:16:09Z
+    	String time = dateTime.getDateTime();
+    	assertEquals(20, time.length());
+    	assertEquals(19, time.indexOf("Z"));
+    	assertEquals(13, time.indexOf(":"));
+    	assertEquals(10, time.indexOf("T"));
+    	assertEquals(4, time.indexOf("-"));
     }
 
     public void testPad() throws Exception {
