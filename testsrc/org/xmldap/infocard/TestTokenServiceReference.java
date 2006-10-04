92a3efb9d52cff577de6a9d6c167830567667ab7
ignisvulpis <ignisvulpis@ae25dcb7-a91b-0410-abf2-913f48ddaf86>
2006-10-04 14:24:01 +0000

    Axel Nennker: fixed the test to support the new namespaces

--- testsrc/org/xmldap/infocard/TestTokenServiceReference.java
+++ testsrc/org/xmldap/infocard/TestTokenServiceReference.java
@@ -13,12 +13,12 @@ import org.xmldap.exceptions.SerializationException;
  * @version 1.0
  */
 public class TestTokenServiceReference extends TestCase {
-    
-    public void setUp() throws Exception {
-        super.setUp();
-    }
-    
-    public void testPad() throws Exception {
+
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testPad() throws Exception {
         X509Certificate cert = null;
         try {
             cert = org.xmldap.util.XmldapCertsAndKeys.getXmldapCert();
@@ -27,11 +27,39 @@ public class TestTokenServiceReference extends TestCase {
             assertFalse(e.getMessage(), true);
         }
         
-        TokenServiceReference tsr = new TokenServiceReference("https://xmldap.org/sts/tokenservice", "https://xmldap.org/sts/mex", cert);
-        tsr.setUserName("cmort");
+        String tsURL = "https://xmldap.org/sts/tokenservice"; 
+        String mexURL = "https://xmldap.org/sts/mex";
+        String userName = "cmort";
+        TokenServiceReference tsr = new TokenServiceReference(tsURL, mexURL, cert);
+        tsr.setUserName(userName);
         
         String actual = null;
-        String expected = "<ic:TokenServiceList xmlns:ic=\"http://schemas.microsoft.com/ws/2005/05/identity\"><ic:TokenService><wsa:EndpointReference xmlns:wsa=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\"><wsa:Address>http://test</wsa:Address><wsid:Identity xmlns:wsid=\"http://schemas.microsoft.com/windows/wcf/2005/09/addressingidentityextension\"><ds:KeyInfo xmlns:ds=\"http://www.w3.org/2000/09/xmldsig#\"><ds:KeyName>Public Key for CN=xmldap.org, OU=infocard, O=xmldap, L=San Francisco, ST=California, C=US</ds:KeyName><ds:KeyValue><ds:RSAKeyValue><ds:Modulus>ANMnkVA4xfpG0bLos9FOpNBjHAdFahy2cJ7FUwuXd/IShnG+5qF/z1SdPWzRxTtpFFyodtXlBUEIbiT+IbYPZF1vCcBrcFa8Kz/4rBjrpPZgllgA/WSVKjnJvw8q4/tO6CQZSlRlj/ebNK9VyT1kN+MrKV1SGTqaIJ2l+7Rd05WHscwZMPdVWBbRrg76YTfy6H/NlQIArNLZanPvE0Vd5QfD4ZyG2hTh3y7ZlJAUndGJ/kfZw8sKuL9QSrh4eOTc280NQUmPGz6LP5MXNmu0RxEcomod1+ToKll90yEKFAUKuPYFgm9J+vYm4tzRequLy/njteRIkcfAdcAtt6PCYjU=</ds:Modulus><ds:Exponent>AQAB</ds:Exponent></ds:RSAKeyValue></ds:KeyValue><ds:X509Data><ds:X509Certificate>MIIDXTCCAkUCBEQd+4EwDQYJKoZIhvcNAQEEBQAwczELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExFjAUBgNVBAcTDVNhbiBGcmFuY2lzY28xDzANBgNVBAoTBnhtbGRhcDERMA8GA1UECxMIaW5mb2NhcmQxEzARBgNVBAMTCnhtbGRhcC5vcmcwHhcNMDYwMzIwMDA0NjU3WhcNMDYwNjE4MDA0NjU3WjBzMQswCQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZyYW5jaXNjbzEPMA0GA1UEChMGeG1sZGFwMREwDwYDVQQLEwhpbmZvY2FyZDETMBEGA1UEAxMKeG1sZGFwLm9yZzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBANMnkVA4xfpG0bLos9FOpNBjHAdFahy2cJ7FUwuXd/IShnG+5qF/z1SdPWzRxTtpFFyodtXlBUEIbiT+IbYPZF1vCcBrcFa8Kz/4rBjrpPZgllgA/WSVKjnJvw8q4/tO6CQZSlRlj/ebNK9VyT1kN+MrKV1SGTqaIJ2l+7Rd05WHscwZMPdVWBbRrg76YTfy6H/NlQIArNLZanPvE0Vd5QfD4ZyG2hTh3y7ZlJAUndGJ/kfZw8sKuL9QSrh4eOTc280NQUmPGz6LP5MXNmu0RxEcomod1+ToKll90yEKFAUKuPYFgm9J+vYm4tzRequLy/njteRIkcfAdcAtt6PCYjUCAwEAATANBgkqhkiG9w0BAQQFAAOCAQEAURtxiA7qDSq/WlUpWpfWiZ7HvveQrwTaTwV/Fk3l/I9e9WIRN51uFLuiLtZMMwR02BX7Yva1KQ/Gl999cm/0b5hptJ+TU29rVPZIlI32c5vjcuSVoEda8+BRj547jlC0rNokyWm+YtBcDOwfHSPFFwVPPVxyQsVEebsiB6KazFq6iZ8A0F2HLEnpsdFnGrSwBBbH3I3PH65ofrTTgj1Mjk5kA6EVaeefDCtlkX2ogIFMlcS6ruihX2mlCLUSrlPs9TH+M4j/R/LV5QWJ93/X9gsxFrxVFGg3b75EKQP8MZ111/jaeKd80mUOAiTO06EtfjXZPrjPN4e2l05i2EGDUA==</ds:X509Certificate></ds:X509Data></ds:KeyInfo></wsid:Identity></wsa:EndpointReference><ic:UserCredential><ic:DisplayCredentialHint>A credential hint</ic:DisplayCredentialHint><ic:UsernamePasswordCredential><ic:Username>cmort</ic:Username></ic:UsernamePasswordCredential></ic:UserCredential></ic:TokenService></ic:TokenServiceList>";
+        String expected = "<ic:TokenServiceList xmlns:ic=\"http://schemas.xmlsoap.org/ws/2005/05/identity\">" + 
+        		"<ic:TokenService><wsa:EndpointReference xmlns:wsa=\"http://www.w3.org/2005/08/addressing\">" + 
+            	"<wsa:Address>" + tsURL + "</wsa:Address><wsa:Metadata>" + 
+        		"<mex:Metadata xmlns:mex=\"http://schemas.xmlsoap.org/ws/2004/09/mex\"><mex:MetadataSection>" + 
+            	"<mex:MetadataReference><wsa:Address>" + mexURL + "</wsa:Address>" + 
+        		"</mex:MetadataReference></mex:MetadataSection></mex:Metadata></wsa:Metadata>" + 
+        		"<wsid:Identity xmlns:wsid=\"http://schemas.xmlsoap.org/ws/2006/02/addressingidentity\">" + 
+            	"<ds:KeyInfo xmlns:ds=\"http://www.w3.org/2000/09/xmldsig#\"><ds:X509Data>" + 
+                "<ds:X509Certificate>MIIDXTCCAkUCBEQd+4EwDQYJKoZIhvcNAQEEBQAwczELMAkGA1UEBhMCVVMxEzA" + 
+        		"RBgNVBAgTCkNhbGlmb3JuaWExFjAUBgNVBAcTDVNhbiBGcmFuY2lzY28xDzANBgNVBAoTBnhtbGRhcDERMA8GA1UECx" + 
+        		"MIaW5mb2NhcmQxEzARBgNVBAMTCnhtbGRhcC5vcmcwHhcNMDYwMzIwMDA0NjU3WhcNMDYwNjE4MDA0NjU3WjBzMQswC" + 
+        		"QYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEWMBQGA1UEBxMNU2FuIEZyYW5jaXNjbzEPMA0GA1UEChMGeG1s" + 
+        		"ZGFwMREwDwYDVQQLEwhpbmZvY2FyZDETMBEGA1UEAxMKeG1sZGFwLm9yZzCCASIwDQYJKoZIhvcNAQEBBQADggEPADC" + 
+        		"CAQoCggEBANMnkVA4xfpG0bLos9FOpNBjHAdFahy2cJ7FUwuXd/IShnG+5qF/z1SdPWzRxTtpFFyodtXlBUEIbiT+Ib" + 
+        		"YPZF1vCcBrcFa8Kz/4rBjrpPZgllgA/WSVKjnJvw8q4/tO6CQZSlRlj/ebNK9VyT1kN+MrKV1SGTqaIJ2l+7Rd05WHs" + 
+        		"cwZMPdVWBbRrg76YTfy6H/NlQIArNLZanPvE0Vd5QfD4ZyG2hTh3y7ZlJAUndGJ/kfZw8sKuL9QSrh4eOTc280NQUmP" + 
+        		"Gz6LP5MXNmu0RxEcomod1+ToKll90yEKFAUKuPYFgm9J+vYm4tzRequLy/njteRIkcfAdcAtt6PCYjUCAwEAATANBgk" + 
+        		"qhkiG9w0BAQQFAAOCAQEAURtxiA7qDSq/WlUpWpfWiZ7HvveQrwTaTwV/Fk3l/I9e9WIRN51uFLuiLtZMMwR02BX7Yv" + 
+        		"a1KQ/Gl999cm/0b5hptJ+TU29rVPZIlI32c5vjcuSVoEda8+BRj547jlC0rNokyWm+YtBcDOwfHSPFFwVPPVxyQsVEe" + 
+        		"bsiB6KazFq6iZ8A0F2HLEnpsdFnGrSwBBbH3I3PH65ofrTTgj1Mjk5kA6EVaeefDCtlkX2ogIFMlcS6ruihX2mlCLUS" + 
+        		"rlPs9TH+M4j/R/LV5QWJ93/X9gsxFrxVFGg3b75EKQP8MZ111/jaeKd80mUOAiTO06EtfjXZPrjPN4e2l05i2EGDUA==" + 
+        		"</ds:X509Certificate></ds:X509Data></ds:KeyInfo></wsid:Identity></wsa:EndpointReference>" + 
+        		"<ic:UserCredential><ic:DisplayCredentialHint>Enter your username and password" + 
+        		"</ic:DisplayCredentialHint><ic:UsernamePasswordCredential><ic:Username>" +
+        		userName + "</ic:Username>" + 
+        		"</ic:UsernamePasswordCredential></ic:UserCredential></ic:TokenService></ic:TokenServiceList>";
         try {
             actual = tsr.toXML();
         } catch (SerializationException e) {
@@ -41,5 +69,4 @@ public class TestTokenServiceReference extends TestCase {
         
         assertEquals(expected, actual);
     }
-    
 }
