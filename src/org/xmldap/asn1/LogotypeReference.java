ed0b28b5c02cb15e21e7c5e2c2901d5cd0d587bc
ignisvulpis <ignisvulpis@ae25dcb7-a91b-0410-abf2-913f48ddaf86>
2006-11-01 10:12:06 +0000

    Axel Nennker: removed "Berkeley" from the license statement.

--- src/org/xmldap/asn1/LogotypeReference.java
+++ src/org/xmldap/asn1/LogotypeReference.java
@@ -10,8 +10,7 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the University of California, Berkeley nor the
- *       names of its contributors may be used to endorse or promote products
+ *     * The names of the contributors may NOT be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
@@ -40,32 +39,34 @@ import org.bouncycastle.asn1.DERSequence;
 import org.bouncycastle.asn1.x509.DigestInfo;
 
 public class LogotypeReference extends ASN1Encodable {
-//	LogotypeReference ::= SEQUENCE {
-//		   refStructHash   SEQUENCE SIZE (1..MAX) OF HashAlgAndValue,
-//		   refStructURI    SEQUENCE SIZE (1..MAX) OF IA5String }
+	// LogotypeReference ::= SEQUENCE {
+	// refStructHash SEQUENCE SIZE (1..MAX) OF HashAlgAndValue,
+	// refStructURI SEQUENCE SIZE (1..MAX) OF IA5String }
 	ASN1Sequence refStructHash = null;
+
 	ASN1Sequence refStructURI = null;
 
-//	public static LogotypeReference getInstance(ASN1TaggedObject obj,
-//			boolean explicit) {
-//		return getInstance(ASN1Sequence.getInstance(obj, explicit));
-//	}
-//
-//	public static LogotypeReference getInstance(Object obj) {
-//		if (obj instanceof LogotypeReference) {
-//			return (LogotypeReference) obj;
-//		} else if (obj instanceof ASN1Sequence) {
-//			return new LogotypeReference((ASN1Sequence) obj);
-//		}
-//
-//		throw new IllegalArgumentException("unknown object in factory");
-//	}
+	// public static LogotypeReference getInstance(ASN1TaggedObject obj,
+	// boolean explicit) {
+	// return getInstance(ASN1Sequence.getInstance(obj, explicit));
+	// }
+	//
+	// public static LogotypeReference getInstance(Object obj) {
+	// if (obj instanceof LogotypeReference) {
+	// return (LogotypeReference) obj;
+	// } else if (obj instanceof ASN1Sequence) {
+	// return new LogotypeReference((ASN1Sequence) obj);
+	// }
+	//
+	// throw new IllegalArgumentException("unknown object in factory");
+	// }
 
 	public static LogotypeReference getInstance(ASN1Sequence seq) {
 		ASN1Sequence refStructHashSeq = null;
 		ASN1Sequence refStructURISeq = null;
 		if (seq.size() != 2) {
-			throw new IllegalArgumentException("size of sequence must be 2 not " + seq.size());
+			throw new IllegalArgumentException(
+					"size of sequence must be 2 not " + seq.size());
 		}
 		refStructHashSeq = ASN1Sequence.getInstance(seq.getObjectAt(0));
 		refStructURISeq = ASN1Sequence.getInstance(seq.getObjectAt(1));
@@ -73,16 +74,18 @@ public class LogotypeReference extends ASN1Encodable {
 		DERIA5String[] refStructURI = null;
 		{
 			Vector<DigestInfo> v = new Vector<DigestInfo>();
-			for (int i=0; i<refStructHashSeq.size(); i++) {
-				DigestInfo di = DigestInfo.getInstance(refStructHashSeq.getObjectAt(i));
+			for (int i = 0; i < refStructHashSeq.size(); i++) {
+				DigestInfo di = DigestInfo.getInstance(refStructHashSeq
+						.getObjectAt(i));
 				v.add(di);
 			}
 			refStructHash = v.toArray(new DigestInfo[refStructHashSeq.size()]);
 		}
 		{
 			Vector<DERIA5String> v = new Vector<DERIA5String>();
-			for (int i=0; i<refStructURISeq.size(); i++) {
-				DERIA5String di = DERIA5String.getInstance(refStructURISeq.getObjectAt(i));
+			for (int i = 0; i < refStructURISeq.size(); i++) {
+				DERIA5String di = DERIA5String.getInstance(refStructURISeq
+						.getObjectAt(i));
 				v.add(di);
 			}
 			refStructHash = v.toArray(new DigestInfo[refStructURISeq.size()]);
@@ -93,11 +96,11 @@ public class LogotypeReference extends ASN1Encodable {
 	public ASN1Sequence getRefStructHash() {
 		return refStructHash;
 	}
-	
+
 	public ASN1Sequence getRefStructURI() {
 		return refStructURI;
 	}
-	
+
 	public LogotypeReference(DigestInfo[] refStructHash,
 			DERIA5String[] refStructURI) {
 		if (refStructHash.length == refStructURI.length) {
