9d90775e32553775cacdbb4ddf64bda9d0f2c1bc
charliemortimore <charliemortimore@ae25dcb7-a91b-0410-abf2-913f48ddaf86>
2006-12-08 16:53:30 +0000

    a minor attempt to see if the computers local name set off less security isseus for janrain

--- firefox/chrome/infocard/content/openid.js
+++ firefox/chrome/infocard/content/openid.js
@@ -1,4 +1,31 @@
-var return_to = "http://127.0.0.1:7055/";
+/*
+ * Copyright (c) 2006, Chuck Mortimore - xmldap.org
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the names xmldap, xmldap.org, xmldap.com nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 
 var verified_id;
 var openid_email;
@@ -8,6 +35,7 @@ var openid_fullname;
 function openid(server){
 
     debug("Starting openid server");
+
     startServer();
 
 
@@ -37,7 +65,10 @@ function openid(server){
         debug("Openid: " + server);
         try{
 
-            var url = server + "?openid.identity=" + openid_url + "&openid.return_to=" + return_to + "&openid.mode=checkid_immediate&openid.sreg.required=email,fullname,nickname";
+            var hostname = java.net.InetAddress.getLocalHost().getHostName();
+            //this doesn't do any good on janrains trust warning
+            //var url = server + "?openid.identity=" + openid_url + "&openid.return_to=http://" + hostname + ":7055/&openid.mode=checkid_immediate&openid.sreg.required=email,fullname,nickname";
+            var url = server + "?openid.identity=" + openid_url + "&openid.return_to=http://localhost:7055/&openid.mode=checkid_immediate&openid.sreg.required=email,fullname,nickname";
             debug('Performing check_immediate: ' + url);
             checkimmediate(url);
 
@@ -162,7 +193,7 @@ function startServer(){
 
     if ( ! serverStarted )  {
         serverSocket = Components.classes["@mozilla.org/network/server-socket;1"].createInstance(Components.interfaces.nsIServerSocket);
-        serverSocket.init(7055,true,-1);
+        serverSocket.init(7055,false,-1);
         serverSocket.asyncListen(async_listener);
         serverStarted = true;
     }
