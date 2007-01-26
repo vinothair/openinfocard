be3f53ac96f2c8f97c90f7291c91b77f6af138f4
ignisvulpis <ignisvulpis@ae25dcb7-a91b-0410-abf2-913f48ddaf86>
2007-01-26 21:03:54 +0000

    Axel Nennker: Language dependend DisplayClaim support for all claims.

--- src/org/xmldap/sts/db/DbSupportedClaim.java
+++ src/org/xmldap/sts/db/DbSupportedClaim.java
@@ -1,5 +1,7 @@
 package org.xmldap.sts.db;
 
+import java.util.Locale;
+
 
 public class DbSupportedClaim {
 	public String uri;
@@ -12,5 +14,23 @@ public class DbSupportedClaim {
 		this.columnName = columnName;
 		this.columnType = columnType;
 		this.displayTags = displayTags;
+	}
+	
+	public String getDisplayTag(Locale clientLocale) {
+		for (DbDisplayTag displayTag : displayTags) {
+			Locale locale;
+			String[] LC = displayTag.language.split("_");
+			String language = LC[0];
+			if (LC.length > 1) {
+				String country = LC[1];
+				locale = new Locale(language, country);
+			} else {
+				locale = new Locale(language);
+			}
+			if (locale.equals(clientLocale)) {
+				return displayTag.displayTag;
+			}
+		}
+		return displayTags[0].displayTag;
 	}
 }
