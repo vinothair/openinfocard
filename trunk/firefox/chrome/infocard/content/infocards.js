a458337a07a28be10d8d34ae29cf79b0877ed7da
ignisvulpis <ignisvulpis@ae25dcb7-a91b-0410-abf2-913f48ddaf86>
2007-07-30 07:50:52 +0000

    Axel Nennker: we now try to retrieve the mex using first http post then http get

--- trunk/firefox/chrome/infocard/content/infocards.js
+++ trunk/firefox/chrome/infocard/content/infocards.js
@@ -121,13 +121,7 @@ function finish(tokenToReturn) {
 
 }
 
-
-
-function processManagedCard(managedCard, requiredClaims) {
-
-    var tokenToReturn = null;
-    var mexResponse;
-
+function getMex(managedCard) {
     var messageIdInt = Math.floor(Math.random()*100000+1);
     var messageId = "urn:uuid:" + messageIdInt;
     var mex = "<s:Envelope " + 
@@ -153,12 +147,37 @@ debug("processManagedCard: mex request: " + mex);
     req.setRequestHeader("User-Agent", "xmldap infocard stack");
     debug('mex xmlhttprequest send');
     req.send(mex);
-debug("processManagedCard: mex request status="+req.status);
+debug("processManagedCard: mex POST request status="+req.status);
     if(req.status == 200) {
-debug("processManagedCard: mex request status 200");
+debug("processManagedCard: mex POST request status 200");
 
         mexResponse = req.responseText;
+        return mexResponse;
+    } else {
+	    req.open('GET', managedCard.carddata.managed.mex, false);
+	    debug('mex xmlhttprequest open');
+	    req.setRequestHeader("Content-type", "application/soap+xml; charset=utf-8");
+	    req.setRequestHeader("Cache-Control", "no-cache");
+	    req.setRequestHeader("accept-language", "en-us");
+	    req.setRequestHeader("User-Agent", "xmldap infocard stack");
+	    debug('mex GET xmlhttprequest send');
+	    req.send(null);
+debug("processManagedCard: mex GET request status="+req.status);
+    if(req.status == 200) {
+debug("processManagedCard: mex GET request status 200");
+        mexResponse = req.responseText;
+        return mexResponse;
+    }
+	return null;
+}
+}
 
+function processManagedCard(managedCard, requiredClaims) {
+
+    var tokenToReturn = null;
+    var mexResponse = getMex(managedCard);
+
+    if(mexResponse != null) {
         //Start with TransportBinding support
         var tb = mexResponse.indexOf("TransportBinding");
         if (tb < 0) {
@@ -287,7 +306,7 @@ debug("processManagedCard: request status 200");
         }
 
     } else {
-    	alert("mex requeste (" + managedCard.carddata.managed.mex + ") failed: " + req.status);
+    	alert("mex request (" + managedCard.carddata.managed.mex + ") failed: " + req.status);
     }
 
     return tokenToReturn;
