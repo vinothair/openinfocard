af6c4bb9188d204a8cbf46df8277066e3723882b
ignisvulpis <ignisvulpis@ae25dcb7-a91b-0410-abf2-913f48ddaf86>
2007-09-11 09:39:27 +0000

    Axel Nennker: new in this theater. Extracted some methods from STSServlet into this class.

---
+++ trunk/src/org/xmldap/sts/servlet/Utils.java
@@ -0,0 +1,227 @@
+package org.xmldap.sts.servlet;
+
+import java.io.IOException;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
+import java.util.Locale;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import nu.xom.Attribute;
+import nu.xom.Element;
+import nu.xom.Nodes;
+import nu.xom.ParsingException;
+import nu.xom.XPathContext;
+
+import org.xmldap.exceptions.SerializationException;
+import org.xmldap.infocard.ManagedToken;
+import org.xmldap.sts.db.DbSupportedClaim;
+import org.xmldap.sts.db.ManagedCard;
+import org.xmldap.sts.db.SupportedClaims;
+import org.xmldap.util.Bag;
+import org.xmldap.util.RandomGUID;
+import org.xmldap.ws.WSConstants;
+import org.xmldap.xmldsig.AsymmetricKeyInfo;
+
+public class Utils {
+
+	static Logger log = Logger.getLogger("org.xmldap.sts.servlet.Utils");
+	
+    public static Bag parseRequest(Element requestXML, String requestURL) throws ParsingException {
+
+        Bag requestElements = new Bag();
+
+
+        XPathContext context = buildRSTcontext();
+
+        Nodes cids = requestXML.query("//wsid:CardId",context);
+        Element cid = (Element) cids.get(0);
+        String cardIdUri = cid.getValue();
+        
+//        String domainname = _su.getDomainName();
+//        String prefix = "https://" + domainname + servletPath + "card/";
+        String cardId = null;
+        if (cardIdUri.startsWith(requestURL+"/card/")) {
+        	cardId = cardIdUri.substring(requestURL.length()+6);
+        } else {
+        	throw new ParsingException("Expected:"+requestURL.toString()+", but found:"+cardIdUri);
+        }
+        log.finest("cardId: " + cardId);
+
+        requestElements.put("cardId", cardId);
+
+
+        Nodes cvs = requestXML.query("//wsid:CardVersion",context);
+        Element cv = (Element) cvs.get(0);
+        String cardVersion = cv.getValue();
+        log.finest("CardVersion: " + cardVersion);
+        requestElements.put("cardVersion", cardVersion);
+
+
+        Nodes claims = requestXML.query("//wsid:ClaimType",context);
+        for (int i = 0; i < claims.size(); i++ ) {
+
+            Element claimElm = (Element)claims.get(i);
+            Attribute uri = claimElm.getAttribute("Uri");
+            String claim = uri.getValue();
+            log.finest("claim:" + claim);
+            requestElements.put("claim", claim);
+
+        }
+
+        Nodes kts = requestXML.query("//wst:KeyType",context);
+        if ( kts != null )  {
+            Element kt = (Element) kts.get(0);
+            String keyType = kt.getValue();
+            log.finest("keyType: " + keyType);
+            requestElements.put("keyType", keyType);
+        }
+            
+
+        Nodes tts = requestXML.query("//wst:TokenType",context);
+        Element tt = (Element) tts.get(0);
+        String tokenType = tt.getValue();
+        log.finest("tokenType: " + tokenType);
+        requestElements.put("tokenType", tokenType);
+
+        return requestElements;
+
+
+    }
+
+    public static String issue(
+    		ManagedCard card, Bag requestElements,  Locale clientLocale, 
+    		X509Certificate cert, RSAPrivateKey key,
+    		String issuer,
+    		SupportedClaims supportedClaimsImpl) throws IOException {
+
+
+        Element envelope = new Element(WSConstants.SOAP_PREFIX + ":Envelope", WSConstants.SOAP12_NAMESPACE);
+        envelope.addNamespaceDeclaration(WSConstants.WSA_PREFIX, WSConstants.WSA_NAMESPACE_05_08);
+        envelope.addNamespaceDeclaration(WSConstants.WSU_PREFIX, WSConstants.WSU_NAMESPACE);
+        envelope.addNamespaceDeclaration(WSConstants.WSSE_PREFIX, WSConstants.WSSE_NAMESPACE_OASIS_10);
+        envelope.addNamespaceDeclaration(WSConstants.TRUST_PREFIX, WSConstants.TRUST_NAMESPACE_05_02);
+        envelope.addNamespaceDeclaration("ic", "http://schemas.xmlsoap.org/ws/2005/05/identity");
+
+        Element header = new Element(WSConstants.SOAP_PREFIX + ":Header", WSConstants.SOAP12_NAMESPACE);
+        Element body = new Element(WSConstants.SOAP_PREFIX + ":Body", WSConstants.SOAP12_NAMESPACE);
+
+
+        envelope.appendChild(header);
+        envelope.appendChild(body);
+
+
+
+        //Build body
+        Element rstr = new Element(WSConstants.TRUST_PREFIX + ":RequestSecurityTokenResponse", WSConstants.TRUST_NAMESPACE_05_02);
+        Attribute context = new Attribute("Context","ProcessRequestSecurityToken");
+        rstr.addAttribute(context);
+
+        Element tokenType = new Element(WSConstants.TRUST_PREFIX + ":TokenType", WSConstants.TRUST_NAMESPACE_05_02);
+        tokenType.appendChild("urn:oasis:names:tc:SAML:1.0:assertion");
+        rstr.appendChild(tokenType);
+
+        Element requestType = new Element(WSConstants.TRUST_PREFIX + ":RequestType", WSConstants.TRUST_NAMESPACE_05_02);
+        requestType.appendChild("http://schemas.xmlsoap.org/ws/2005/02/trust/Issue");
+        rstr.appendChild(requestType);
+
+        Element rst = new Element(WSConstants.TRUST_PREFIX + ":RequestedSecurityToken", WSConstants.TRUST_NAMESPACE_05_02);
+
+        AsymmetricKeyInfo keyInfo = new AsymmetricKeyInfo(cert);
+        ManagedToken token = new ManagedToken(keyInfo,key);
+
+        Set<String> cardClaims = card.getClaims();
+        for (String claim : cardClaims) {
+        	token.setClaim(claim, card.getClaim(claim));
+        }
+        
+        token.setPrivatePersonalIdentifier(card.getPrivatePersonalIdentifier());
+        token.setValidityPeriod(-3, 10);
+        token.setIssuer(issuer);
+        
+        RandomGUID uuid = new RandomGUID();
+
+        try {
+            rst.appendChild(token.serialize(uuid));
+        } catch (SerializationException e) {
+            e.printStackTrace();
+        }
+
+        rstr.appendChild(rst);
+
+        Element requestedAttachedReference = new Element(WSConstants.TRUST_PREFIX + ":RequestedAttachedReference", WSConstants.TRUST_NAMESPACE_05_02);
+        Element securityTokenReference = new Element(WSConstants.WSSE_PREFIX + ":SecurityTokenReference", WSConstants.WSSE_NAMESPACE_OASIS_10);
+        Element keyIdentifier = new Element(WSConstants.WSSE_PREFIX + ":KeyIdentifier", WSConstants.WSSE_NAMESPACE_OASIS_10);
+        Attribute valueType = new Attribute("ValueType","http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.0#SAMLAssertionID");
+        keyIdentifier.addAttribute(valueType);
+        keyIdentifier.appendChild("uuid-" + uuid.toString());
+        securityTokenReference.appendChild(keyIdentifier);
+        requestedAttachedReference.appendChild(securityTokenReference);
+        rstr.appendChild(requestedAttachedReference);
+
+        Element requestedUnAttachedReference = new Element(WSConstants.TRUST_PREFIX + ":RequestedUnattachedReference", WSConstants.TRUST_NAMESPACE_05_02);
+        Element securityTokenReference1 = new Element(WSConstants.WSSE_PREFIX + ":SecurityTokenReference", WSConstants.WSSE_NAMESPACE_OASIS_10);
+        Element keyIdentifier1 = new Element(WSConstants.WSSE_PREFIX + ":KeyIdentifier", WSConstants.WSSE_NAMESPACE_OASIS_10);
+        Attribute valueType1 = new Attribute("ValueType","http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.0#SAMLAssertionID");
+        keyIdentifier1.addAttribute(valueType1);
+        keyIdentifier1.appendChild("uuid-" + uuid.toString());
+        securityTokenReference1.appendChild(keyIdentifier1);
+        requestedUnAttachedReference.appendChild(securityTokenReference1);
+        rstr.appendChild(requestedUnAttachedReference);
+
+
+        Element requestedDisplayToken = new Element(WSConstants.INFOCARD_PREFIX + ":RequestedDisplayToken", WSConstants.INFOCARD_NAMESPACE);
+        Element displayToken = new Element(WSConstants.INFOCARD_PREFIX + ":DisplayToken", WSConstants.INFOCARD_NAMESPACE);
+        Attribute lang = new Attribute("xml:lang","http://www.w3.org/XML/1998/namespace","en");
+        displayToken.addAttribute(lang);
+        requestedDisplayToken.appendChild(displayToken);
+
+        for (String uri : cardClaims) {
+        	String value = card.getClaim(uri);
+        	DbSupportedClaim dbClaim = supportedClaimsImpl.getClaimByUri(uri);
+        	String displayTag = dbClaim.getDisplayTag(clientLocale);
+            addDisplayClaim(
+            		displayToken, 
+            		uri, 
+            		displayTag, 
+            		value);
+        }
+
+        addDisplayClaim(displayToken, org.xmldap.infocard.Constants.IC_NAMESPACE, "PPID", card.getPrivatePersonalIdentifier());
+
+        rstr.appendChild(requestedDisplayToken);
+
+        body.appendChild(rstr);
+
+        return envelope.toXML();
+    }
+
+	public static void addDisplayClaim(Element displayToken, String claimUri, String claimName, String claimValue) {
+		Element displayClaim = new Element(WSConstants.INFOCARD_PREFIX + ":DisplayClaim", WSConstants.INFOCARD_NAMESPACE);
+        Attribute uri = new Attribute("Uri", claimUri);
+        displayClaim.addAttribute(uri);
+        Element displayTag = new Element(WSConstants.INFOCARD_PREFIX + ":DisplayTag", WSConstants.INFOCARD_NAMESPACE);
+        displayTag.appendChild(claimName);
+        Element displayValue = new Element(WSConstants.INFOCARD_PREFIX + ":DisplayValue", WSConstants.INFOCARD_NAMESPACE);
+        displayValue.appendChild(claimValue);
+        displayClaim.appendChild(displayTag);
+        displayClaim.appendChild(displayValue);
+        displayToken.appendChild(displayClaim);
+	}
+	
+	/**
+	 * @return an XPathContext suitable to parse a RST 
+	 */
+	private static XPathContext buildRSTcontext() {
+		XPathContext context = new XPathContext();
+        context.addNamespace("s",WSConstants.SOAP12_NAMESPACE);
+        context.addNamespace("a", WSConstants.WSA_NAMESPACE_05_08);
+        context.addNamespace("wst", "http://schemas.xmlsoap.org/ws/2005/02/trust");
+        context.addNamespace("wsid","http://schemas.xmlsoap.org/ws/2005/05/identity");
+        context.addNamespace("o","http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
+        context.addNamespace("u","http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd");
+		return context;
+	}
+
+
+}
