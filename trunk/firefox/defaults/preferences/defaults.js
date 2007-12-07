5f2204beb845d4e81789eb84b4ad172c20f57f97
ignisvulpis <ignisvulpis@ae25dcb7-a91b-0410-abf2-913f48ddaf86>
2007-12-07 13:42:56 +0000

    Axel Nennker: forgot to commit these. sorry.

---
+++ trunk/firefox/defaults/preferences/defaults.js
@@ -0,0 +1,5 @@
+pref("extensions.infocard.cardStoreMasterPasswordEncryption", true);
+pref("extensions.infocard.cardStoreCurrentProfile", true);
+pref("extensions.infocard.cardStoreLocalFilePath", "");
+pref("extensions.infocard.cardStoreUrl", "");
+pref("identityselector.contractid", "@xmldap.org/identityselector;1");
