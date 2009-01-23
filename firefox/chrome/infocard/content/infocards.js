561967990619f91aa17b3c8af2d25c312aa169e9
ignisvulpis <ignisvulpis@ae25dcb7-a91b-0410-abf2-913f48ddaf86>
2009-01-23 21:01:36 +0000

    Axel Nennker: moved the cardManager's debug functions into their own file

--- firefox/chrome/infocard/content/infocards.js
+++ firefox/chrome/infocard/content/infocards.js
@@ -1417,20 +1417,3 @@ function processCard(policy, enableDebug){
     return token;
 
 }
-
-function icDebug2(msg, width) {
-	  var debug = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
-	  var message = "";
-	  while (msg.length > width) {
-		  message += msg.substr(0,width) + "\n";
-		  msg = msg.substring(width);
-	  }
-	  if (msg.length > 0) {
-		  message += msg;
-	  }
-	  debug.logStringMessage("infocard: " + message);
-}
-function icDebug(msg) {
-	  var debug = Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService);
-	  debug.logStringMessage("infocard: " + msg);
-}
