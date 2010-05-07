fc6dfc4f0dcc8598371b1f0d51bcca92cb5466fd
ignisvulpis <ignisvulpis@ae25dcb7-a91b-0410-abf2-913f48ddaf86>
2010-05-07 20:30:35 +0000

    Axel Nennker: global variable gMissingPluginInstalleris new in Firefox 3.7 and replaces missingPluginInstaller. This fix is not complete and has to be worked on when the Firefox release candidates are here

--- trunk/firefox/chrome/infocard/content/missingPluginInstaller.js
+++ trunk/firefox/chrome/infocard/content/missingPluginInstaller.js
@@ -1,16 +1,38 @@
-// store the original
-missingPluginInstaller.prototype.__newMissingPlugin = missingPluginInstaller.prototype.newMissingPlugin;
 
-missingPluginInstaller.prototype.newMissingPlugin = function(aEvent){
-	var pluginInfo = getPluginInfo(aEvent.target);
-	if ("application/x-informationcard" == pluginInfo.mimetype) {
-		var doc = aEvent.target.ownerDocument;
-		var objElem = aEvent.target;
-		// send event
-		var event = doc.createEvent( "Event");
-        event.initEvent( "ICObjectLoaded", true, true);
-        objElem.dispatchEvent( event);
-		return;
-	}
-	missingPluginInstaller.prototype.__newMissingPlugin(aEvent);
-};
+if (typeof(missingPluginInstaller) !== "undefind") {
+  // store the original
+  missingPluginInstaller.prototype.__newMissingPlugin = missingPluginInstaller.prototype.newMissingPlugin;
+  
+  missingPluginInstaller.prototype.newMissingPlugin = function(aEvent){
+  	var pluginInfo = getPluginInfo(aEvent.target);
+  	if ("application/x-informationcard" == pluginInfo.mimetype) {
+  		var doc = aEvent.target.ownerDocument;
+  		var objElem = aEvent.target;
+  		// send event
+  		var event = doc.createEvent( "Event");
+          event.initEvent( "ICObjectLoaded", true, true);
+          objElem.dispatchEvent( event);
+  		return;
+  	}
+  	missingPluginInstaller.prototype.__newMissingPlugin(aEvent);
+  };
+}
+
+if (typeof(gMissingPluginInstaller) !== "undefined") {
+  // store the original
+  gMissingPluginInstaller.prototype.__newMissingPlugin = gMissingPluginInstaller.prototype.newMissingPlugin;
+  
+  gMissingPluginInstaller.prototype.newMissingPlugin = function(aEvent){
+    var pluginInfo = getPluginInfo(aEvent.target);
+    if ("application/x-informationcard" == pluginInfo.mimetype) {
+      var doc = aEvent.target.ownerDocument;
+      var objElem = aEvent.target;
+      // send event
+      var event = doc.createEvent( "Event");
+          event.initEvent( "ICObjectLoaded", true, true);
+          objElem.dispatchEvent( event);
+      return;
+    }
+    gMissingPluginInstaller.prototype.__newMissingPlugin(aEvent);
+  };
+}
