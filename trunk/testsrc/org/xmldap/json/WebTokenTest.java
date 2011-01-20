674b46b3d9fb967d5e0c2b18f94ef6fe3bab06c3
ignisvulpis@gmail.com <ignisvulpis@gmail.com@ae25dcb7-a91b-0410-abf2-913f48ddaf86>
2011-01-20 15:24:57 +0000

    Axel Nennker: - JSON Web Token now with verification of signatures - JWT now with 384 and 512 bit algoritms support

--- trunk/testsrc/org/xmldap/json/WebTokenTest.java
+++ trunk/testsrc/org/xmldap/json/WebTokenTest.java
@@ -1,9 +1,17 @@
 package org.xmldap.json;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.InvalidKeyException;
+import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 
 import junit.framework.TestCase;
 
@@ -19,6 +27,7 @@ import org.bouncycastle.math.ec.ECCurve;
 import org.bouncycastle.math.ec.ECPoint;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.xmldap.json.WebToken;
 import org.xmldap.util.Base64;
 
 public class WebTokenTest extends TestCase {
@@ -30,8 +39,19 @@ public class WebTokenTest extends TestCase {
   
   String hs256 = "{\"typ\":\"JWT\",\r\n"+
                  " \"alg\":\"HS256\"}";
+  String hs384 = "{\"typ\":\"JWT\",\r\n"+
+                 " \"alg\":\"HS384\"}";
+  String hs512 = "{\"typ\":\"JWT\",\r\n"+
+                 " \"alg\":\"HS512\"}";
+  byte[] hsKey = {3, (byte)35, (byte)53, (byte)75, (byte)43, (byte)15, (byte)165, (byte)188, (byte)131, (byte)126, (byte)6, (byte)101, (byte)119, (byte)123, (byte)166, (byte)143, (byte)90, (byte)179, (byte)40, (byte)230, (byte)240, (byte)84, (byte)201, (byte)40, (byte)169, (byte)15, (byte)132, (byte)178, (byte)210, (byte)80, (byte)46, (byte)191, (byte)211, (byte)251, (byte)90, (byte)146, (byte)210, (byte)6, (byte)71, (byte)239, (byte)150, (byte)138, (byte)180, (byte)195, (byte)119, (byte)98, (byte)61, (byte)34, (byte)61, (byte)46, (byte)33, (byte)114, (byte)5, (byte)46, (byte)79, (byte)8, (byte)192, (byte)205, (byte)154, (byte)245, (byte)103, (byte)208, (byte)128, (byte)163};
   
   String es256 = "{\"alg\":\"ES256\"}";
+  String es384 = "{\"alg\":\"ES384\"}";
+  String es512 = "{\"alg\":\"ES512\"}";
+
+  String rs256 = "{\"alg\":\"RS256\"}";
+  String rs384 = "{\"alg\":\"RS384\"}";
+  String rs512 = "{\"alg\":\"RS512\"}";
   
   public void setUp() {
     try {
@@ -81,68 +101,91 @@ public class WebTokenTest extends TestCase {
     byte[] d = {(byte)243, (byte)189, 12, 7, (byte)168, 31, (byte)185, 50, 120, 30, (byte)213, 39, 82, (byte)246, 12, (byte)200, (byte)154, 107, (byte)229, (byte)229, 25, 52, (byte)254, 1, (byte)147, (byte)141, (byte)219, 85, (byte)216, (byte)247, 120, 1};
     
 //    "secp256r1 [NIST P-256, X9.62 prime256v1]", "1.2.840.10045.3.1.7"
-    DERObjectIdentifier oid = SECObjectIdentifiers.secp256r1;
-    X9ECParameters x9ECParameters = SECNamedCurves.getByOID(oid);
-    ECDomainParameters ecParameterSpec = new ECDomainParameters(
-        x9ECParameters.getCurve(), 
-        x9ECParameters.getG(), 
-        x9ECParameters.getN(), 
-        x9ECParameters.getH(), 
-        x9ECParameters.getSeed());
-    assertNotNull(ecParameterSpec);
-    ECPrivateKeyParameters ecPrivateKeyParameters = new ECPrivateKeyParameters(
-        new BigInteger(1,d), ecParameterSpec);
     WebToken wt = new WebToken(joeStr, es256);
-    String signed = wt.serialize(ecPrivateKeyParameters);
+    String signed = wt.serialize(new BigInteger(1,d));
     String[] split = signed.split("\\.");
     assertEquals(3, split.length);
     assertEquals("eyJhbGciOiJFUzI1NiJ9", split[0]);
     assertEquals("eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ", split[1]);
     byte[] signatureBytes = Base64.decodeUrl(split[2]);
     assertEquals(64, signatureBytes.length);
-    byte[] rBytes = new byte[32];
-    System.arraycopy(signatureBytes, 0, rBytes, 0, 32);
-    byte[] sBytes = new byte[32];
-    System.arraycopy(signatureBytes, 32, sBytes, 0, 32);
-    
-    BigInteger r = new BigInteger(1, rBytes);
-    BigInteger s = new BigInteger(1, sBytes);
-    
-    {
-      ECDSASigner verifier = new ECDSASigner();
-      byte[] x = {48, (byte)160, 66, 76, (byte)210, 28, 41, 68, (byte)131, (byte)138, 45, 117, (byte)201, 43, 55, (byte)231, 110, (byte)162, 13, (byte)159, 0, (byte)137, 58, 59, 78, (byte)238, (byte)138, 60, 10, (byte)175, (byte)236, 62};
-      byte[] y = {(byte)224, 75, 101, (byte)233, 36, 86, (byte)217, (byte)136, (byte)139, 82, (byte)179, 121, (byte)189, (byte)251, (byte)213, 30, (byte)232, 105, (byte)239, 31, 15, (byte)198, 91, 102, 89, 105, 91, 108, (byte)206, 8, 23, 35};
-      BigInteger xB = new BigInteger(1, x);
-      BigInteger yB = new BigInteger(1, y);
-      ECCurve curve = ecPrivateKeyParameters.getParameters().getCurve();
-      ECPoint qB = curve.createPoint(xB, yB, false);
-      ECPoint q = new ECPoint.Fp(curve, qB.getX(), qB.getY());
-      ECDomainParameters ecDomainParameters = new ECDomainParameters(
-          curve, 
-          ecPrivateKeyParameters.getParameters().getG(), 
-          ecPrivateKeyParameters.getParameters().getN(), 
-          ecPrivateKeyParameters.getParameters().getH(),
-          ecPrivateKeyParameters.getParameters().getSeed());
-      ECPublicKeyParameters ecPublicKeyParameters = new ECPublicKeyParameters(
-          q, ecDomainParameters);
-      verifier.init(false, ecPublicKeyParameters);
-      boolean verified = verifier.verifySignature(joeStr.getBytes("utf-8"), r, s);
-      assertTrue(verified);
-      System.out.println("Verfied: " + verified);
-    }
+    byte[] x = {48, (byte)160, 66, 76, (byte)210, 28, 41, 68, (byte)131, (byte)138, 45, 117, (byte)201, 43, 55, (byte)231, 110, (byte)162, 13, (byte)159, 0, (byte)137, 58, 59, 78, (byte)238, (byte)138, 60, 10, (byte)175, (byte)236, 62};
+    byte[] y = {(byte)224, 75, 101, (byte)233, 36, 86, (byte)217, (byte)136, (byte)139, 82, (byte)179, 121, (byte)189, (byte)251, (byte)213, 30, (byte)232, 105, (byte)239, 31, 15, (byte)198, 91, 102, 89, 105, 91, 108, (byte)206, 8, 23, 35};
+    assertTrue(WebToken.verify(signed, x, y));
 
   }
   
-  public void test1() throws InvalidKeyException, NoSuchAlgorithmException, IllegalStateException, UnsupportedEncodingException, JSONException {
-    WebToken jwt = new WebToken(joeStr, hs256);
-    byte[] key = {83, (byte)159, 117, 12, (byte)235, (byte)169, (byte)168, (byte)200, (byte)131, (byte)152, (byte)227, (byte)246, (byte)214, (byte)212, (byte)188, 74, 71, 83, (byte)244, (byte)166, 90, 24, (byte)239, (byte)251, 32, 124, 6, (byte)201, (byte)194, 104, (byte)241, 62, (byte)174, (byte)246, 65, 111, 49, 52, (byte)210, 118, (byte)212, 124, 34, 88, (byte)167, 112, 84, 88, 83, 65, (byte)155, 18, (byte)234, (byte)250, (byte)224, 101, (byte)147, (byte)221, 23, 104, (byte)219, (byte)170, (byte)146, (byte)215};
-    String signed = jwt.serialize(key);
+  private void testHMACSHA(
+      String jwtAlgorithm, String jwtHeaderSegment, String jwtCryptoSegment) 
+  throws InvalidKeyException, NoSuchAlgorithmException, IllegalStateException, UnsupportedEncodingException, JSONException {
+    WebToken jwt = new WebToken(joeStr, jwtAlgorithm);
+    String signed = jwt.serialize(hsKey);
     String[] split = signed.split("\\.");
     assertEquals(3, split.length);
-    assertEquals("eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9", split[0]);
+    assertEquals(jwtHeaderSegment, split[0]);
     assertEquals("eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ", split[1]);
-    assertEquals("35usWj9X8HwGS-CDcx1JP2NmqcrLwZ4EKp8sNThf3cY", split[2]);
-    String expected = "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.35usWj9X8HwGS-CDcx1JP2NmqcrLwZ4EKp8sNThf3cY";
+    assertEquals(jwtCryptoSegment, split[2]);
+    String expected = split[0] + "." + split[1] + "." + split[2];
     assertEquals(expected, signed);
   }
+  
+  public void testHS256() throws InvalidKeyException, NoSuchAlgorithmException, IllegalStateException, UnsupportedEncodingException, JSONException {
+    testHMACSHA(hs256, "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9", "dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk"); 
+  }
+
+  public void testHS384() throws InvalidKeyException, NoSuchAlgorithmException, IllegalStateException, UnsupportedEncodingException, JSONException {
+    testHMACSHA(hs384, "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzM4NCJ9", 
+        "TUfcA4Xjq_veopvw1fiFG99UswFSMvxYisxxBb0kHQ7w8He3OkvmELPo0uy3RuR0"); 
+  }
+  
+  public void testHS512() throws InvalidKeyException, NoSuchAlgorithmException, IllegalStateException, UnsupportedEncodingException, JSONException {
+    testHMACSHA(hs512, "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzUxMiJ9", 
+        "iXxB-yPnHRvriuSAfTrwz-gr5WYC6tg7gIq9JndRI9Uqn4D6twBgsJuQsQks6WqAC6OB23Lvdht79p_lA6jE8g"); 
+  }
+  
+  private void testRSASHA(
+      String jwtAlgorithm, String jwtHeaderSegment, String jwtCryptoSegment) 
+  throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, SignatureException, JSONException, IOException {
+    WebToken jwt = new WebToken(joeStr, jwtAlgorithm);
+    byte[] n = {(byte)161, (byte)248, (byte)22, (byte)10, (byte)226, (byte)227, (byte)201, (byte)180, (byte)101, (byte)206, (byte)141, (byte)45, (byte)101, (byte)98, (byte)99, (byte)54, (byte)43, (byte)146, (byte)125, (byte)190, (byte)41, (byte)225, (byte)240, (byte)36, (byte)119, (byte)252, (byte)22, (byte)37, (byte)204, (byte)144, (byte)161, (byte)54, (byte)227, (byte)139, (byte)217, (byte)52, (byte)151, (byte)197, (byte)182, (byte)234, (byte)99, (byte)221, (byte)119, (byte)17, (byte)230, (byte)124, (byte)116, (byte)41, (byte)249, (byte)86, (byte)176, (byte)251, (byte)138, (byte)143, (byte)8, (byte)154, (byte)220, (byte)75, (byte)105, (byte)137, (byte)60, (byte)193, (byte)51, (byte)63, (byte)83, (byte)237, (byte)208, (byte)25, (byte)184, (byte)119, (byte)132, (byte)37, (byte)47, (byte)236, (byte)145, (byte)79, (byte)228, (byte)133, (byte)119, (byte)105, (byte)89, (byte)75, (byte)234, (byte)66, (byte)128, (byte)211, (byte)44, (byte)15, (byte)85, (byte)191, (byte)98, (byte)148, (byte)79, (byte)19, (byte)3, (byte)150, (byte)188, (byte)110, (byte)155, (byte)223, (byte)110, (byte)189, (byte)210, (byte)189, (byte)163, (byte)103, (byte)142, (byte)236, (byte)160, (byte)198, (byte)104, (byte)247, (byte)1, (byte)179, (byte)141, (byte)191, (byte)251, (byte)56, (byte)200, (byte)52, (byte)44, (byte)226, (byte)254, (byte)109, (byte)39, (byte)250, (byte)222, (byte)74, (byte)90, (byte)72, (byte)116, (byte)151, (byte)157, (byte)212, (byte)185, (byte)207, (byte)154, (byte)222, (byte)196, (byte)199, (byte)91, (byte)5, (byte)133, (byte)44, (byte)44, (byte)15, (byte)94, (byte)248, (byte)165, (byte)193, (byte)117, (byte)3, (byte)146, (byte)249, (byte)68, (byte)232, (byte)237, (byte)100, (byte)193, (byte)16, (byte)198, (byte)182, (byte)71, (byte)96, (byte)154, (byte)164, (byte)120, (byte)58, (byte)235, (byte)156, (byte)108, (byte)154, (byte)215, (byte)85, (byte)49, (byte)48, (byte)80, (byte)99, (byte)139, (byte)131, (byte)102, (byte)92, (byte)111, (byte)111, (byte)122, (byte)130, (byte)163, (byte)150, (byte)112, (byte)42, (byte)31, (byte)100, (byte)27, (byte)130, (byte)211, (byte)235, (byte)242, (byte)57, (byte)34, (byte)25, (byte)73, (byte)31, (byte)182, (byte)134, (byte)135, (byte)44, (byte)87, (byte)22, (byte)245, (byte)10, (byte)248, (byte)53, (byte)141, (byte)154, (byte)139, (byte)157, (byte)23, (byte)195, (byte)64, (byte)114, (byte)143, (byte)127, (byte)135, (byte)216, (byte)154, (byte)24, (byte)216, (byte)252, (byte)171, (byte)103, (byte)173, (byte)132, (byte)89, (byte)12, (byte)46, (byte)207, (byte)117, (byte)147, (byte)57, (byte)54, (byte)60, (byte)7, (byte)3, (byte)77, (byte)111, (byte)96, (byte)111, (byte)158, (byte)33, (byte)224, (byte)84, (byte)86, (byte)202, (byte)229, (byte)233, (byte)161};
+    byte[] e = {1, 0, 1};
+    byte[] d = {18, (byte)174, (byte)113, (byte)164, (byte)105, (byte)205, (byte)10, (byte)43, (byte)195, (byte)126, (byte)82, (byte)108, (byte)69, (byte)0, (byte)87, (byte)31, (byte)29, (byte)97, (byte)117, (byte)29, (byte)100, (byte)233, (byte)73, (byte)112, (byte)123, (byte)98, (byte)89, (byte)15, (byte)157, (byte)11, (byte)165, (byte)124, (byte)150, (byte)60, (byte)64, (byte)30, (byte)63, (byte)207, (byte)47, (byte)44, (byte)211, (byte)189, (byte)236, (byte)136, (byte)229, (byte)3, (byte)191, (byte)198, (byte)67, (byte)155, (byte)11, (byte)40, (byte)200, (byte)47, (byte)125, (byte)55, (byte)151, (byte)103, (byte)31, (byte)82, (byte)19, (byte)238, (byte)216, (byte)193, (byte)90, (byte)37, (byte)216, (byte)213, (byte)206, (byte)160, (byte)2, (byte)94, (byte)227, (byte)171, (byte)46, (byte)139, (byte)127, (byte)121, (byte)33, (byte)111, (byte)198, (byte)59, (byte)234, (byte)86, (byte)39, (byte)83, (byte)180, (byte)6, (byte)68, (byte)198, (byte)161, (byte)81, (byte)39, (byte)217, (byte)178, (byte)149, (byte)69, (byte)64, (byte)160, (byte)187, (byte)225, (byte)163, (byte)5, (byte)86, (byte)152, (byte)45, (byte)78, (byte)159, (byte)222, (byte)95, (byte)100, (byte)37, (byte)241, (byte)77, (byte)75, (byte)113, (byte)52, (byte)65, (byte)181, (byte)93, (byte)199, (byte)59, (byte)155, (byte)74, (byte)237, (byte)204, (byte)146, (byte)172, (byte)227, (byte)146, (byte)126, (byte)55, (byte)245, (byte)125, (byte)12, (byte)253, (byte)94, (byte)117, (byte)129, (byte)250, (byte)81, (byte)44, (byte)143, (byte)73, (byte)97, (byte)169, (byte)235, (byte)11, (byte)128, (byte)248, (byte)168, (byte)7, (byte)70, (byte)114, (byte)138, (byte)85, (byte)255, (byte)70, (byte)71, (byte)31, (byte)52, (byte)37, (byte)6, (byte)59, (byte)157, (byte)83, (byte)100, (byte)47, (byte)94, (byte)222, (byte)30, (byte)132, (byte)214, (byte)19, (byte)8, (byte)26, (byte)250, (byte)92, (byte)34, (byte)208, (byte)81, (byte)40, (byte)91, (byte)214, (byte)59, (byte)148, (byte)59, (byte)86, (byte)93, (byte)137, (byte)138, (byte)5, (byte)104, (byte)84, (byte)19, (byte)229, (byte)60, (byte)60, (byte)108, (byte)101, (byte)37, (byte)255, (byte)31, (byte)227, (byte)78, (byte)61, (byte)220, (byte)112, (byte)240, (byte)213, (byte)100, (byte)80, (byte)253, (byte)164, (byte)139, (byte)161, (byte)46, (byte)16, (byte)78, (byte)157, (byte)235, (byte)159, (byte)184, (byte)24, (byte)129, (byte)225, (byte)196, (byte)189, (byte)242, (byte)93, (byte)146, (byte)71, (byte)244, (byte)80, (byte)200, (byte)101, (byte)146, (byte)121, (byte)104, (byte)231, (byte)115, (byte)52, (byte)244, (byte)65, (byte)79, (byte)117, (byte)167, (byte)80, (byte)225, (byte)57, (byte)84, (byte)110, (byte)58, (byte)138, (byte)115, (byte)157};
+    
+    BigInteger N = new BigInteger(1, n);
+    BigInteger E = new BigInteger(1, e);
+    BigInteger D = new BigInteger(1, d);
+    
+    KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+    RSAPublicKeySpec pubKeySpec = new RSAPublicKeySpec(N, E);
+    RSAPrivateKeySpec privKeySpec = new RSAPrivateKeySpec(N, D);
+
+    RSAPublicKey pubKey = (RSAPublicKey) keyFactory.generatePublic(pubKeySpec);
+    RSAPrivateKey privKey = (RSAPrivateKey) keyFactory.generatePrivate(privKeySpec);
+    
+    String signed = jwt.serialize(privKey);
+    String[] split = signed.split("\\.");
+    
+    assertEquals(3, split.length);
+    assertEquals(jwtHeaderSegment, split[0]);
+    String jwtPayloadSegment = "eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ";
+    assertEquals(jwtPayloadSegment, split[1]);
+    assertEquals(jwtCryptoSegment, split[2]);
+    
+    String expected = jwtHeaderSegment + "." + jwtPayloadSegment + "." + jwtCryptoSegment;
+    assertEquals(expected, signed);
+
+  }
+
+  public void testRS256() throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, SignatureException, JSONException, IOException {
+    String jwtHeaderSegment = "eyJhbGciOiJSUzI1NiJ9";
+    String jwtCryptoSegment = "cC4hiUPoj9Eetdgtv3hF80EGrhuB__dzERat0XF9g2VtQgr9PJbu3XOiZj5RZmh7AAuHIm4Bh-0Qc_lF5YKt_O8W2Fp5jujGbds9uJdbF9CUAr7t1dnZcAcQjbKBYNX4BAynRFdiuB--f_nZLgrnbyTyWzO75vRK5h6xBArLIARNPvkSjtQBMHlb1L07Qe7K0GarZRmB_eSN9383LcOLn6_dO--xi12jzDwusC-eOkHWEsqtFZESc6BfI7noOPqvhJ1phCnvWh6IeYI2w9QOYEUipUTI8np6LbgGY9Fs98rqVt5AXLIhWkWywlVmtVrBp0igcN_IoypGlUPQGe77Rw";
+    testRSASHA(rs256, jwtHeaderSegment, jwtCryptoSegment);
+  }
+  
+  public void testRS384() throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, SignatureException, JSONException, IOException {
+    String jwtHeaderSegment = "eyJhbGciOiJSUzM4NCJ9";
+    String jwtCryptoSegment = "UqgNjrJOGhk4wfoSG6Uvrt9GcKu-TgPwInExALrMBadg1pol1uTw7mZADTddAWsC6ZzdFiTFUmIi7DuD38ftLAZoW4qezdAO7RYf1yZDsbT20bt8DJJN1I4VovL2PLg80B6x6ug-kaW8k5LaM5ce0dk1zgWhjafKC3Mb4UNLL8f9fqVMkHpdWYRjF6QjTz12Ap-gq-tPyUoWSdvzCIYOcZ9-08SQQdUTTgsNF1Qwu3TqeWPqzNJwmWHiHMmaV8I4ktMFEX-AiEBa55KsfYTx0jSbTHP-odqmnLQJ4n-oQJ2RSXy0HQP6BkdiwDHdoMUk4z_wAeOsfDTs_mLxTgOInQ";
+    testRSASHA(rs384, jwtHeaderSegment, jwtCryptoSegment);
+  }
+
 }
